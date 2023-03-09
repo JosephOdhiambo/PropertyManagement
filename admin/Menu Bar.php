483b1965dab1b5d56dcdfd0d9b467f7713ac9df7
Joseph Modi <odhisjoseph85@gmail.com>
2023-03-09 15:22:00 +0300

    Modify Menu Bar
    It was only displaying the login functionality

--- admin/Menu Bar.php
+++ admin/Menu Bar.php
@@ -9,13 +9,12 @@ error_reporting(1);
             <li><a class="nav-link scrollto" href="../index.php" title="Home">Home</a></li>
             <li><a class="nav-link scrollto" href="../about.php" title="About">About</a></li>
             <li><a class="nav-link scrollto" href="../image gallery.php" title="Gallery">Gallery</a></li>
-        </ul>
-        <ul>
             <?php 
             if(isset($_SESSION['create_account_logged_in']) && $_SESSION['create_account_logged_in']!="")
             {
             ?>
-            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">View Status <span class="caret"></span></a>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">View Status <span
+                        class="caret"></span></a>
                 <ul class="dropdown-menu">
                     <li><a href="profile.php">Profile</a></li>
                     <li><a href="order.php">Booking Status</a></li>
@@ -25,11 +24,13 @@ error_reporting(1);
             <?PHP } else
             {
             ?>
-            <li><a class="nav-link scrollto" href="../Login.php" title="login"><span class="glyphicon glyphicon-log-in"></span>&nbsp;&nbsp;User Login</a>
+            <li><a class="nav-link scrollto" href="../Login.php" title="login"><span
+                        class="glyphicon glyphicon-log-in"></span>&nbsp;&nbsp;User Login</a>
             </li>
             <?php 
             } ?>
-            <li><a class="nav-link scrollto" href="index.php" title="Admin Login"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;Admin Login</a></li>
+            <li><a class="nav-link scrollto" href="index.php" title="Admin Login"><span
+                        class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;Admin Login</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
     </nav><!-- .navbar -->
